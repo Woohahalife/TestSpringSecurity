019ba8e7e03c8e229c9c952e1e85a1338f880bec
Geon.C <chlrjs1324@gmail.com>
2024-01-26 00:00:35 +0900

    시큐리티 로그인 시 세션 접근해 사용자 정보 추출 실습
    : SecurityContextHolder 통해 관리되는 서버 세션에 접근
    
    세션 관련 설정 방법 실습
    
    : 세션 소멸 시간 설정, 중복 로그인 설정, 고정 세션 보호 방법

--- src/main/java/com/core/testspringsecurity/config/SecurityConfig.java
+++ src/main/java/com/core/testspringsecurity/config/SecurityConfig.java
@@ -27,10 +27,29 @@ public class SecurityConfig {
 
                 .csrf(auth -> auth.disable());
 
-        http
-                .formLogin(auth -> auth.loginPage("/login") // 로그인 페이지 url 지정(접근권한 없는 url 요청시 로그인 페이지로 리다이렉트)
-                        .loginProcessingUrl("/loginProc") // 로그인 데이터 처리 url 지정
-                        .permitAll()
+        http // 로그인 폼 관련 설정(커스텀 로그인)
+                .formLogin(
+                        auth -> auth
+                                .loginPage("/login") // 로그인 페이지 url 지정(접근권한 없는 url 요청시 로그인 페이지로 리다이렉트)
+                                .loginProcessingUrl("/loginProc") // 로그인 데이터 처리 url 지정
+                                .permitAll()
+                );
+
+        http // 다중 로그인 설정
+                .sessionManagement(
+                        auth -> auth
+                                .maximumSessions(1) // 한 아이디당 중복 로그인 허용 개수 설정
+                                .maxSessionsPreventsLogin(true) // 중복 로그인 개수 초과시 처리방법
+                        // (true : 새로운 로그인 차단 / false : 초과시 기존 세션 하나 삭제)
+                );
+
+        http // 세션 고정 보호 : 세션 쿠키 탈취를 통해 user 정보가 탈취될 수 있는 세션 고정 공격을 보호하기 위한 방법
+                // sessionManagement().sessionFixation().none() : 로그인 시 세션 정보 변경 안함
+                // sessionManagement().sessionFixation().newSession() : 로그인 시 세션 새로 생성
+                // sessionManagement().sessionFixation().changeSessionId() : 로그인 시 동일한 세션에 대한 id 변경(JSESSIONID 변경)
+                // sessionManagement().sessionFixation().migrateSession() : 로그인 시 새로운 세션 생성 및 기존 세션을 새로운 세션으로 마이그레이션(디폴트)
+                .sessionManagement(
+                        auth -> auth.sessionFixation().changeSessionId()
                 );
         return http.build();
     }
--- src/main/java/com/core/testspringsecurity/controller/MainController.java
+++ src/main/java/com/core/testspringsecurity/controller/MainController.java
@@ -1,13 +1,38 @@
 package com.core.testspringsecurity.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String MainP() {
+    public String MainP(Model model) {
+
+        // 로그인 진행 후 사용자 정보는 SecurityContextHolder에 의해 서버 세션에 관리됨
+
+        // 시큐리티에서 로그인한 사용자에 대해 세션에 저장되어있는 아이디를 확인
+        String id = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        // 시큐리티에서 로그인한 사용자에 대해 세션에 저장되어있는 role을 확인
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+
+        System.out.println("role = " + role);
+
+        model.addAttribute("id", id);
+        model.addAttribute("role", role);
         return "main";
     }
 }
