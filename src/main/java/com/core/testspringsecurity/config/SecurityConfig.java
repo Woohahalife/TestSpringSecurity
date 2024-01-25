db25fc994ec8f9da6bdf844e66856baa420b8c65
Geon.C <chlrjs1324@gmail.com>
2024-01-25 16:09:19 +0900

    http.formLogin()을 통한 폼 기반 로그인 페이지 활성화 연습
    .loginPage("/url") : 로그인 페이지 url 지정
    .loginProcessingUrl("/url") : 로그인 데이터 처리 url 지정
    
    로그인 페이지 지정시 접근권한 없는 페이지로 접근했을 때 접근 거부가 아닌 로그인 페이지로 리다이렉트

--- src/main/java/com/core/testspringsecurity/config/SecurityConfig.java
+++ src/main/java/com/core/testspringsecurity/config/SecurityConfig.java
@@ -17,9 +17,19 @@ public class SecurityConfig {
                 .authorizeHttpRequests(
                         auth -> auth
                                 .requestMatchers("/", "login").permitAll()
-                                .requestMatchers("/admin").hasRole("ADMIN")
+                                .requestMatchers("/admin").hasRole("ADMIN") // 로그인 성공해도 role 해당 안될시 자원을 받지못함
                                 .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
                                 .anyRequest().authenticated()
+                );
+
+        http// csrf보호 비활성화 : 로그인 시 csrf도 같이 보내줘야 하지만 개발환경에서는 불편하기에 일단 비활성화함
+
+                .csrf(auth -> auth.disable());
+
+        http
+                .formLogin(auth -> auth.loginPage("/login") // 로그인 페이지 url 지정(접근권한 없는 url 요청시 로그인 페이지로 리다이렉트)
+                        .loginProcessingUrl("/loginProc") // 로그인 데이터 처리 url 지정
+                        .permitAll()
                 );
 
         return http.build();
---
+++ src/main/java/com/core/testspringsecurity/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.core.testspringsecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String loginP() {
+
+        return "login";
+    }
+
+}
